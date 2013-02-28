39b1cb36820d7ab8b6cf508f68a6d0ea394a382a
Paul Blouët <paul@paulblouet.fr>
2013-02-28 18:54:20 +0100

    Working on Rule
    This is WIP

--- src/main/java/fr/paulblouet/criojomin/Rule.scala
+++ src/main/java/fr/paulblouet/criojomin/Rule.scala
@@ -1,5 +1,19 @@
 package fr.paulblouet.criojomin
 
+/*
+If we have (x, y, z): R(x, y + 1) & S(y) --> T(z) | z < y ? R(x, y) & S(y)
+We want to rewrite it as:
+(x: Var[Any], y: Var[Int], z: Var[Int]) => (R(x, {what?}
+*/
 trait Rule {
+  val arity: Int
+  val premise: Premise
+  val guard: Guard
+
+  //val conclusion: Iterable
+
+  trait Premise
+
+  trait Guard
 
 }
