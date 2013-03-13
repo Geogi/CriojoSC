dec2c7158a3dd3378768bc15d5b2153efb2c1675
Paul Blouët <paul@paulblouet.fr>
2013-03-13 14:27:59 +0100

    Removed in-Solution engine
    (see a26bec59ae5f1aa45ef7cc5a2df430997bfee163)

--- src/main/java/fr/emn/criojosc/Solution.scala
+++ src/main/java/fr/emn/criojosc/Solution.scala
@@ -19,41 +19,8 @@
 
 package fr.emn.criojosc
 
-import collection.mutable
-
-class Solution(val agent: Agent) {
-  val content = mutable.HashSet.empty[ClosedReactant]
-
-  def +=(reactant: ClosedReactant) {
-    content += reactant
-    val completed_states = agent.rules map (rule => (rule, rule.premise += reactant))
-    // dummy choice algorithm beginning
-    val selected: Option[(Rule, State)] = completed_states filter (_._2.nonEmpty) match {
-      case r :: _ => Some((r._1, r._2.head))
-      case _ => None
-    }
-    // dummy choice algorithm end
-    selected foreach {
-      rule_state =>
-        rule_state._2.right foreach (this -= _)
-        rule_state._1.conclusion(rule_state._2.s).content foreach (this += _)
-    }
-  }
-
-  def ++=(rs: Iterable[ClosedReactant]) {
-    rs.foreach(this += _)
-  }
-
-  def -=(reactant: ClosedReactant) {
-    content -= reactant
-    agent.rules foreach (_.premise -= reactant)
-  }
-}
+class Solution(val content: Set[ClosedReactant])
 
 object Solution {
-  def apply(agent: Agent, rs: ClosedReactant*) = {
-    val sol = new Solution(agent)
-    sol ++= rs
-    sol
-  }
+  def apply(rs: ClosedReactant*) = new Solution(rs.toSet)
 }
