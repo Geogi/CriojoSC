507cb91e544df1b59ee217d40efd4d1dbe705120
Paul Blouët <paul@paulblouet.fr>
2013-03-27 18:06:58 +0100

    Should use the name if that's possible

--- src/main/scala/fr/emn/criojosc/experimental/PrettyPrinter.scala
+++ src/main/scala/fr/emn/criojosc/experimental/PrettyPrinter.scala
@@ -20,8 +20,25 @@
 package fr.emn.criojosc
 package experimental
 
-import reflect.runtime.{universe => ru}
+import reflect.runtime.universe._
 
 class PrettyPrinter(val agents: Iterable[Agent]) extends Engine {
+  def print: String = agents.zipWithIndex.map(printAgent).mkString("\n")
 
+  private def printAgent(args: (Agent, Int)): String = {
+    val (agent, index) = args
+    val names = (agent.relations ++ agent.channels).zipWithIndex.map(t => (t._1, genStringName(t._2))).toMap
+    "Agent " + index + ":\n" + agent.rules.map("  " + printRule(_, names))
+  }
+
+  private def printRule(rule: Rule, entityNames: Map[EntitySymbol, String]): String = {
+    //getType(rule).members.filter(_.isMethod).map(_.asMethod).map(_.)
+    ""
+  }
+
+  private def genStringName(index: Int): String =
+    if (index < 26) (index + 65).toChar.toString
+    else genStringName(index / 26 - 1) + genStringName(index % 26)
+
+  private def getType[T: TypeTag](obj: T) = typeTag[T].tpe
 }
