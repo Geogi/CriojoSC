1d336c0a8998d186b62b12eba0f376a48212f5d4
Paul Blouët <paul@paulblouet.fr>
2013-04-08 17:35:19 +0200

    Adds HTML output, removes specs2 index hack
    The hack doesn't work anymore for some reason, removing it.

--- build.sbt
+++ build.sbt
@@ -7,6 +7,4 @@ scalaVersion in ThisBuild := "2.10.1"
 libraryDependencies in ThisBuild ++= Seq("org.specs2" %% "specs2" % "1.14" % "test",
                                          "org.pegdown" % "pegdown" % "1.2.1" % "test")
 
-testOptions in Test += Tests.Argument("html")
-
-javaOptions in Compile += "-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M"
+testOptions in Test in ThisBuild += Tests.Argument("html", "console")
--- main/src/test/scala/fr/emn/criojosc/index.scala
+++ main/src/test/scala/fr/emn/criojosc/index.scala
@@ -20,69 +20,8 @@
 package fr.emn.criojosc
 
 import org.specs2._
-import main.Arguments
 import runner.SpecificationsFinder._
-import java.util.regex.{Matcher, Pattern => JavaPattern}
-import specification.SpecificationStructure
 
 class index extends Specification {
   def is = "index".title ^ specifications().map(link)
-
-  def specifications(path: String = "**/*.scala",
-                     pattern: String = ".*Spec",
-                     filter: String => Boolean = { (name: String) => true },
-                     basePath: String = "src/test/scala",
-                     verbose: Boolean = false)
-                    (implicit args: Arguments = Arguments()): Seq[SpecificationStructure] = {
-    specificationNames(path, pattern, basePath, verbose).view.filter(filter).
-      flatMap(n => createSpecification(n, verbose))
-  }
-
-  def specificationNames(path: String = "**/*.scala",
-                         pattern: String = ".*Spec",
-                         basePath: String = "src/test/scala",
-                         verbose: Boolean = false) : Seq[String] = {
-    lazy val specClassPattern = {
-      val p = specPattern("class", pattern)
-      if (verbose) println("\nthe pattern used to match specification classes is: "+p+"\n")
-      JavaPattern.compile(p)
-    }
-    lazy val specObjectPattern = {
-      val p = specPattern("object", pattern)
-      if (verbose) println("\nthe pattern used to match specification objects is: "+p+"\n")
-      JavaPattern.compile(p)
-    }
-    filePaths(basePath, path, verbose) filter (_.endsWith(".scala")) flatMap { p =>
-      val fileContent = readFile(p)
-      val packName = packageName(fileContent)
-      val (objectPattern, classPattern) = (specObjectPattern, specClassPattern)
-      if (verbose) println("\nSearching for specifications in file: "+p)
-      classNames(packName, fileContent, objectPattern, "$", verbose) ++ classNames(packName, fileContent, classPattern, "", verbose)
-    }
-  }
-
-  def classNames(packageName: String, content: String, pattern: JavaPattern, suffix: String, verbose: Boolean = false): Seq[String] = {
-    val CLASSNAME_REGEX = """([\p{L}_$][\p{L}\p{N}_$]*\.)*[\p{L}_$][\p{L}\p{N}_$]*""".r
-    def result(m: Matcher): Stream[String] =
-      if (m.find) {
-        val fullName = List(packageName, m.group(1).trim).mkString("") + suffix
-        Stream.cons(fullName, result(m))
-      } else Stream.empty
-
-    val found = result(pattern.matcher(content)).toList
-    if (verbose && found.nonEmpty) println("found the following classes: "+found.mkString(", "))
-    found.filter(c => CLASSNAME_REGEX.pattern.matcher(c).matches)
-  }
-
-  def packageName(content: String): String = {
-    def result(m: Matcher): Stream[String] =
-      if (m.find) Stream.cons(m.group(1).replace(";", "").trim, result(m))
-      else Stream.empty
-
-    val pattern = "\\s*package\\s*(.+)\\s*"
-
-    // extract the packages section at the beginning of the file
-    val packages = content.split("\n").filter(_.trim.startsWith("package")).mkString("\n")
-    result(JavaPattern.compile(pattern).matcher(packages)).mkString("")
-  }
 }
