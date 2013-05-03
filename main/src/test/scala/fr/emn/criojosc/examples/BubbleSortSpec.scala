d75a829d2a4c7e6a3f5f2d7e316f028ebfcd333d
Paul Blouët <paul@paulblouet.fr>
2013-05-03 15:59:40 +0200

    done

--- main/src/test/scala/fr/emn/criojosc/examples/BubbleSortSpec.scala
+++ main/src/test/scala/fr/emn/criojosc/examples/BubbleSortSpec.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2013 Mines Nantes.
+ *
+ * This file is part of CriojoSC.
+ *
+ * CriojoSC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CriojoSC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CriojoSC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.emn.criojosc
 package examples
 
@@ -6,55 +25,31 @@ import fr.emn.criojosc.automaton.VerboseEngine
 
 class BubbleSortSpec extends Specification { def is =
   "Test CriojoSC with the transitive closure algorithm"         ^
-    p^
-    automatonOut                                                  ^
-    end
+                                                                p^
+  automatonOut                                                  ^
+                                                                end
 
-  lazy val transitiveClosure = new Agent {
-    override val name = Some("TransitiveClosure")
+  lazy val bubbleSort = new Agent {
+    override val name = Some("BubbleSort")
 
-    lazy val R = Relation[String, String]("R")
-    lazy val Rs = Relation[String, String]("R+")
-    val solution = new Solution(Set(R("A", "B"), R("B", "C"), R("C", "A")))
+    lazy val R = Relation[Int, Int]("R")
+    val solution = new Solution(Set(R(0, 3), R(1, 7), R(2, 1)))
     val rules = Seq(
       new Rule {
-        override val name = Some("Init")
-        lazy val (x, y) = (Variable[String]("x"), Variable[String]("y"))
-        def conclusion(implicit s: Valuation) = new Conclusion(List(Rs(!x, !y), R(!x, !y)))
-        override val explicitVal = Some("R+(x, y) & R(x, y)")
+        override val name = Some("Sort")
+        lazy val (i, j, u, v) = (Variable[Int]("i"), Variable[Int]("j"), Variable[Int]("u"), Variable[Int]("v"))
+        def conclusion(implicit s: Valuation) = new Conclusion(List(R(!i, !v), R(!j, !u)))
+        override val explicitVal = Some("R(i, v) & R(j, u))")
 
-        val premise = new Premise(List(R?(x, y)))
-        val guard = ! new ControlGuard {
-          override val name = Some("Init — no duplicate")
-          lazy val (u, v) = (Variable[String]("u"), Variable[String]("v"))
-          val premise = new Premise(List(Rs?(u, v)))
-          val guard = new NativeGuard({implicit s: Valuation => !x == !u && !y == !v}) {
-            override val explicitVal = Some("x == u && y == v")
-          }
-        }
-      },
-      new Rule {
-        override val name = Some("Extend")
-        lazy val (x, y, u, v) = (Variable[String]("x"), Variable[String]("y"), Variable[String]("u"), Variable[String]("v"))
-        def conclusion(implicit s: Valuation) = new Conclusion(List(Rs(!x, !v), Rs(!u, !v), Rs(!x, !y)))
-        override val explicitVal = Some("R+(x, v) & R+(y, v) & R+(x, y)")
-
-        val premise = new Premise(List(Rs?(x, y), Rs?(u, v)))
-        val guard: Guard = new NativeGuard({implicit s: Valuation => !y == !u}) {
-          override val explicitVal = Some("y == u")
-        } && ! new ControlGuard {
-          override val name = Some("Extend — no duplicate")
-          lazy val (i, j) = (Variable[String]("i"), Variable[String]("j"))
-          val premise = new Premise(List(Rs?(i, j)))
-          val guard = new NativeGuard({implicit s: Valuation => !i == !x && !j == !v}) {
-            override val explicitVal = Some("i == x && j == v")
-          }
+        val premise = new Premise(List(R?(i, u), R?(j, v)))
+        val guard = ! new NativeGuard({implicit s: Valuation => !j == !i + 1}) {
+          override val explicitVal = Some("j == i + 1")
         }
       }
     )
   }
-  lazy val engine = new VerboseEngine(List(transitiveClosure))
-  engine.MAX_ITS = Some(15)
+  lazy val engine = new VerboseEngine(List(bubbleSort))
+  engine.MAX_ITS = Some(100)
 
   lazy val automatonOut = {
     val stream = new java.io.ByteArrayOutputStream()
--- main/src/test/scala/fr/emn/criojosc/examples/TransitiveClosureSpec.scala
+++ main/src/test/scala/fr/emn/criojosc/examples/TransitiveClosureSpec.scala
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2013 Mines Nantes.
- *
- * This file is part of CriojoSC.
- *
- * CriojoSC is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * CriojoSC is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with CriojoSC.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package fr.emn.criojosc
 package examples
 
@@ -25,31 +6,55 @@ import fr.emn.criojosc.automaton.VerboseEngine
 
 class TransitiveClosureSpec extends Specification { def is =
   "Test CriojoSC with the transitive closure algorithm"         ^
-                                                                p^
-  automatonOut                                                  ^
-                                                                end
+    p^
+    automatonOut                                                  ^
+    end
 
-  lazy val bubbleSort = new Agent {
-    override val name = Some("BubbleSort")
+  lazy val transitiveClosure = new Agent {
+    override val name = Some("TransitiveClosure")
 
-    lazy val R = Relation[Int, Int]("R")
-    val solution = new Solution(Set(R(0, 3), R(1, 7), R(2, 1)))
+    lazy val R = Relation[String, String]("R")
+    lazy val Rs = Relation[String, String]("R+")
+    val solution = new Solution(Set(R("A", "B"), R("B", "C"), R("C", "A")))
     val rules = Seq(
       new Rule {
-        override val name = Some("Sort")
-        lazy val (i, j, u, v) = (Variable[Int]("i"), Variable[Int]("j"), Variable[Int]("u"), Variable[Int]("v"))
-        def conclusion(implicit s: Valuation) = new Conclusion(List(R(!i, !v), R(!j, !u)))
-        override val explicitVal = Some("R(i, v) & R(j, u))")
+        override val name = Some("Init")
+        lazy val (x, y) = (Variable[String]("x"), Variable[String]("y"))
+        def conclusion(implicit s: Valuation) = new Conclusion(List(Rs(!x, !y), R(!x, !y)))
+        override val explicitVal = Some("R+(x, y) & R(x, y)")
 
-        val premise = new Premise(List(R?(i, u), R?(j, v)))
-        val guard = ! new NativeGuard({implicit s: Valuation => !j == !i + 1}) {
-          override val explicitVal = Some("j == i + 1")
+        val premise = new Premise(List(R?(x, y)))
+        val guard = ! new ControlGuard {
+          override val name = Some("Init — no duplicate")
+          lazy val (u, v) = (Variable[String]("u"), Variable[String]("v"))
+          val premise = new Premise(List(Rs?(u, v)))
+          val guard = new NativeGuard({implicit s: Valuation => !x == !u && !y == !v}) {
+            override val explicitVal = Some("x == u && y == v")
+          }
+        }
+      },
+      new Rule {
+        override val name = Some("Extend")
+        lazy val (x, y, u, v) = (Variable[String]("x"), Variable[String]("y"), Variable[String]("u"), Variable[String]("v"))
+        def conclusion(implicit s: Valuation) = new Conclusion(List(Rs(!x, !v), Rs(!u, !v), Rs(!x, !y)))
+        override val explicitVal = Some("R+(x, v) & R+(y, v) & R+(x, y)")
+
+        val premise = new Premise(List(Rs?(x, y), Rs?(u, v)))
+        val guard: Guard = new NativeGuard({implicit s: Valuation => !y == !u}) {
+          override val explicitVal = Some("y == u")
+        } && ! new ControlGuard {
+          override val name = Some("Extend — no duplicate")
+          lazy val (i, j) = (Variable[String]("i"), Variable[String]("j"))
+          val premise = new Premise(List(Rs?(i, j)))
+          val guard = new NativeGuard({implicit s: Valuation => !i == !x && !j == !v}) {
+            override val explicitVal = Some("i == x && j == v")
+          }
         }
       }
     )
   }
-  lazy val engine = new VerboseEngine(List(bubbleSort))
-  engine.MAX_ITS = Some(100)
+  lazy val engine = new VerboseEngine(List(transitiveClosure))
+  engine.MAX_ITS = Some(15)
 
   lazy val automatonOut = {
     val stream = new java.io.ByteArrayOutputStream()
